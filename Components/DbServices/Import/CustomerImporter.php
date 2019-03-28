513ac347028d60dd31f13ddd250b5d2c19d6c804
Steffen Wirth <wirth.steffen.mail@gmail.com>
2019-03-28 11:42:18 +0100

    Revert "set customernumber to use magento customer id"
    This reverts commit 888ceceae43efbe43393eb022ddaf3a78777acbf.

--- Components/DbServices/Import/CustomerImporter.php
+++ Components/DbServices/Import/CustomerImporter.php
@@ -292,14 +292,12 @@ class CustomerImporter
                 }
             }
 
-//            $customer['customernumber'] = $this->getCustomerNumber($customer['userID']);
-            $customer['customernumber'] = $customer['userID'];
+            $customer['customernumber'] = $this->getCustomerNumber($customer['userID']);
 
             $customer = $this->newsletterSubscribe($customer, $quotedCustomerEmail);
         } else {
             $customer['userID'] = $isCustomer;
-//            $customer['customernumber'] = $this->getCustomerNumber($customer['userID']);
-            $customer['customernumber'] = $customer['userID'];
+            $customer['customernumber'] = $this->getCustomerNumber($customer['userID']);
 
             $customer = $this->newsletterSubscribe($customer, $quotedCustomerEmail);
         }
